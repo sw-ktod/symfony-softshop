5df10d08d2e8fd26e8a825c55b4acc15535aa263
Kristiyan Todorov <sw.ktod@gmail.com>
2017-04-29 21:28:40 +0300

    fix: product add form style

--- src/ShopBundle/Form/ProductType.php
+++ src/ShopBundle/Form/ProductType.php
@@ -5,7 +5,9 @@ namespace ShopBundle\Form;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,17 +18,47 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')
-            ->add('description')
-            ->add('price')
+        $builder->add('name', TextType::class, [
+            'label' => 'Name',
+            'attr' => [
+                'class' => 'form-control',
+                'id' => 'name',
+                'type' => 'text'
+            ]
+        ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'description',
+                    'type' => 'text'
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Price',
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'price',
+                    'type' => 'money'
+                ]
+            ])
             ->add('category', EntityType::class, [
                 'class' => 'ShopBundle:Category',
                 'choice_label' => function ($category) {
                     return $category->getName();
                 },
-                'label' => 'Category'
+                'label' => 'Category',
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'category'
+                ]
             ])
-            ->add('submit', SubmitType::class);
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary pull-right',
+                    'style' => 'margin-top: 10px;'
+                ]
+            ]);
     }
 
     /**
