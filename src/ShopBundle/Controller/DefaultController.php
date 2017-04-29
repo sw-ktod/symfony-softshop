6590d99d164d5ae35507e9f5d878c7df714939c4
Kristiyan Todorov <sw.ktod@gmail.com>
2017-04-29 22:16:28 +0300

    fix: list products

--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace ShopBundle\Controller;
 
 use Proxies\__CG__\ShopBundle\Entity\Category;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use ShopBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -16,8 +17,11 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+
         return [
-            'categories' => $categories
+            'categories' => $categories,
+            'products' => $products
         ];
     }
 }
