e38c5ab6eb1ff79c9d53819bd7c6cfde787d9bdc
Kristiyan Todorov <sw.ktod@gmail.com>
2017-04-29 19:30:04 +0300

    fix: reset migrations

--- app/DoctrineMigrations/Version20170429132931.php
+++ app/DoctrineMigrations/Version20170429132931.php
@@ -8,13 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170425201800 extends AbstractMigration
+class Version20170429132931 extends AbstractMigration
 {
     /**
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
+        // this up() migration is auto-generated, please modify it to your needs
 
     }
 
